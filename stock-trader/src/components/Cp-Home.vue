b1e7f2c7fb951dcf9d2eb443d68e30037ccebd28
CURSOSDEVS <claudisneibello@hotmail.com>
2022-07-16 18:08:11 -0300

    Adicionando validações nos componentes cp-stock e cp-stock-portfolio para vender e comprar ações de acordo com o saldo disponível e a quantidade de ações disponíveis

--- stock-trader/src/components/Cp-Home.vue
+++ stock-trader/src/components/Cp-Home.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
         <h1 class="display-3 font-weight-light mb-4">Negocie e Consulte suas Ações</h1>
-        <v-sheet :elevation="6" class="pa-4 primary" >
-            <v-icon class="white--text mr-3" role="img">info</v-icon>
+   
+        <v-alert :elevation="6" type="info" class="white--text mr-3 pa-4 primary">
             <span class="headline white--text font-weight-light">Você pode Salvar & Carregar os Dados</span>
-        </v-sheet>
-        <v-sheet :elevation="6" class="pa-4 success darken-1 mt-3" >
-            <v-icon class="white--text mr-3" role="img">info</v-icon>
+        </v-alert>   
+       
+        <v-alert :elevation="6" type="info" class="pa-4 mt-3 success darken-1 white--text mr-3">
             <span class="headline white--text font-weight-light">Você pode Salvar & Carregar os Dados</span>
-        </v-sheet>
+        </v-alert>
+            
         <v-divider class="my-4"></v-divider>
         <p class="display-1"><strong>Seu Saldo :</strong>{{ funds}}</p>
     </div>
